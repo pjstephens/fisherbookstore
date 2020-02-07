2833ce6ced6837a63f47105f866e7abcd4b4de6c
Paige Stephens <stephens.658@osu.edu>
2020-02-07 18:26:50 -0500

    home

---
+++ src/Homes/Home.js
@@ -0,0 +1,13 @@
+import React from "./node_modules/react";
+import "./Home.css";
+
+export default function Home() {
+    return(
+        <div className="Home">
+            <div className="lander">
+                <h1>Home</h1>
+                <p>Welcome to Hisher Bookstore</p>
+            </div>
+        </div>
+    );
+}
